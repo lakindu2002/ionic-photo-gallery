af82eeb8149e6187259f1689194bd0cdc3e58f06
lakindu hewawasam <lakindu2002@live.com>
2022-08-01 22:20:21 +0530

    added a camera icon

--- src/pages/gallery-tab.tsx
+++ src/pages/gallery-tab.tsx
@@ -1,11 +1,28 @@
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonFab, IonFabButton, IonIcon, IonPage } from '@ionic/react';
 import { Header } from '../components/header/header';
+import { camera } from 'ionicons/icons';
+import React, { FC } from 'react';
 
-const GalleryTab: React.FC = () => {
+const GalleryTab: FC = () => {
+
+  const handleOnCameraClick = () => {
+    console.log('clicked');
+  };
+
   return (
     <IonPage>
       <Header title='Photo Gallery' />
       <IonContent fullscreen={false}>
+        <IonFab
+          vertical='bottom'
+          horizontal='center'
+        >
+          <IonFabButton
+            onClick={handleOnCameraClick}
+          >
+            <IonIcon icon={camera} />
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
