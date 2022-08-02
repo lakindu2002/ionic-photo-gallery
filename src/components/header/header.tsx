ae0210683afc4ad6bb6236e1bb525b5a79abef27
lakindu hewawasam <lakindu2002@live.com>
2022-08-02 22:53:06 +0530

    updated header to support end icons

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,27 @@
 import { IonHeader, IonTitle, IonToolbar } from "@ionic/react";
-import { FC } from "react";
+import { FC, Fragment } from "react";
 
 interface HeaderProps {
   title: string;
+  endIcon?: JSX.Element
 }
 
 export const Header: FC<HeaderProps> = (props) => {
-  const { title } = props;
+  const { title, endIcon } = props;
   return (
     <>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>
-            {title}
-          </IonTitle>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <IonTitle>
+              {title}
+            </IonTitle>
+            {endIcon && (
+              <Fragment>
+                {endIcon}
+              </Fragment>
+            )}
+          </div>
         </IonToolbar>
       </IonHeader>
     </>
